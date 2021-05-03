9fbda805dc77b6ca36ffb771495b72a3389b20b8
Gautam Kumar <gautam958@gmail.com>
2021-05-03 10:06:44 +0800

    Update Heroku web api

--- src/Services/Masters/UserService.js
+++ src/Services/Masters/UserService.js
@@ -3,8 +3,8 @@ import axios from "axios";
 
 //const roadmapApiUrl = 'http://10.39.180.230/TrafficSystemSpeedMapAPI/api/v1/roadmap';
 //const roadmapApiUrl = 'http://192.168.128.119/TrafficSystemSpeedMapAPI/api/v1/roadmap';
-const ApiUrl = 'http://10.39.181.74/dailyworkApi/api/v1/users';
-//const ApiUrl = 'https://dailyworknodeapi.herokuapp.com/api/users';
+//const ApiUrl = 'http://10.39.181.74/dailyworkApi/api/v1/users';
+const ApiUrl = 'https://dailyworknodeapi.herokuapp.com/api/users';
 const options = {
     headers: {
         'Accept': 'application/json',
